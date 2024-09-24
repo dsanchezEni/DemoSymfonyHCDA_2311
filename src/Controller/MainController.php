502286fea4c6b502e6ba47ec05edd266dc727d20
dsanchez <dsanchez@campus-eni.fr>
2024-09-24 09:55:38 +0200

    Demo module 3 Demo 1 et 2 Twig: les variables et les filtres

---
+++ src/Controller/MainController.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/main')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig');
+    }
+    #[Route('/', name: 'main_home', methods: ['GET'])]
+    public function home(): Response
+    {
+        return new Response("<h1>Accueil</h1>");
+    }
+
+    #[Route('/test', name: 'main_test', methods: ['GET'])]
+    public function test(): Response
+    {
+        /*return new Response("<h1>Test</h1>");*/
+        return $this->render('main/test.html.twig');
+    }
+}
