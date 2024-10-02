83d2cb9354e5ebfef991fa915daa0f0ba1c1c4c5
dsanchez <dsanchez@campus-eni.fr>
2024-10-02 12:14:04 +0200

    Demo module 9 Demo 2- Mise en place d'un service SenderService.php Ajout d'un service de Mail dans le service

--- src/Notification/SenderService.php
+++ src/Notification/SenderService.php
@@ -2,12 +2,26 @@
 
 namespace App\Notification;
 
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class SenderService
 {
+
+    public function __construct(private readonly MailerInterface $mailer)
+    {
+    }
+
     public function sendNewUserNotificationToAdmin(UserInterface $user):void{
         //Pour tester
         file_put_contents("debug.txt",$user->getEmail());
+
+        $message = new Email();
+        $message->from('account@demo.com')
+            ->to('admin@test.fr')
+            ->subject('Nouvel utilisateur créé depuis le site de démo')
+        ->html('<h1>Nouvel Utilisateur:</h1><p>Email : '.$user->getEmail().'</p>');
+        $this->mailer->send($message);
     }
 }
