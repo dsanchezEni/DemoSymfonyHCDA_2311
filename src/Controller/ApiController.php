097de5f24a273a1b5c754d4a8821e4b14da97601
dsanchez <dsanchez@campus-eni.fr>
2024-10-03 09:52:37 +0200

    Demo module 10 Demo 4- Mise en place d'une API de type CRUD sur category

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Models\Region;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends AbstractController
@@ -35,4 +39,72 @@ class ApiController extends AbstractController
        // return new JsonResponse($resultat, Response::HTTP_OK,[],true);
         return $this->json($categories, Response::HTTP_OK,[],['groups'=>['getCategoriesFull']]);
     }
+    #[Route('/api/categories/{id}', name: 'api_category_read',requirements:['id'=>'\d+'],methods: ['GET'])]
+    public function read(?Category $category, SerializerInterface $serializer):JsonResponse{
+        if(!$category){
+            return new JsonResponse(null,Response::HTTP_NOT_FOUND);
+        }
+        return $this->json($category, Response::HTTP_OK,[],['groups'=>['getCategoriesFull']]);
+
+    }
+
+    #[Route('/api/categories', name: 'api_category_create',methods: ['POST'])]
+    public function create(Request $request, SerializerInterface $serializer,EntityManagerInterface $em):JsonResponse{
+       //On crée une category
+        //On récupère les données dans la requête.
+        $data = $request->getContent();
+        $category = $serializer->deserialize($data, Category::class,'json');
+        $em->persist($category);
+        $em->flush();
+        //On retourne une réponse avec le code statut 201 et la nouvelle catégorie au format JSON.
+        return $this->json($category, Response::HTTP_CREATED,[
+            "Location"=>$this->generateUrl('api_category_read',
+                ["id"=>$category->getId()],
+                UrlGeneratorInterface::ABSOLUTE_URL)
+        ],
+            ['groups'=>'getCategoriesFull']
+        );
+    }
+
+    #[Route('/api/categories/{id}', name: 'api_category_update',requirements:['id'=>'\d+'],methods: ['PUT'])]
+    public function update(?Category $category,Request $request,
+                           SerializerInterface $serializer,EntityManagerInterface $em):JsonResponse{
+        //On test si on a la category
+        if(!$category){
+            return new JsonResponse(null,Response::HTTP_NOT_FOUND);
+        }
+        //On récupère les données dans la requête.
+        $data = $request->getContent();
+        //On injecte les données reçues dans l'objet $category récupéré
+        $serializer->deserialize($data,
+            Category::class,
+            'json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE=>$category]);
+        $em->flush();
+        //On retourne une réponse avec le code statut 204 sans données.
+        return new JsonResponse(null,Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/api/categories/{id}', name: 'api_category_delete',requirements:['id'=>'\d+'],methods: ['DELETE'])]
+    public function delete(?Category $category,EntityManagerInterface $em):JsonResponse{
+        //On test si on a la category
+        if(!$category){
+            return new JsonResponse(null,Response::HTTP_NOT_FOUND);
+        }
+        $em->remove($category);
+        $em->flush();
+        //On retourne une réponse avec le code statut 204 sans données.
+        return new JsonResponse(null,Response::HTTP_NO_CONTENT);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
